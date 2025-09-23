81f361b4df2a9d6fb1e6b248ea7f723ee59ed829
zcdev <zcdev@hotmail.com>
2025-09-23 08:11:05 -0700

    style: implement overall UI look and feel - Implement mobile-first responsive design - Add typewriter animation - Add spark animation - Add cookie slip flip transition effect - Add background gradient and sf tech building scene image - Refine Tailwind CSS classes on App page and semantic layout

--- app/page.tsx
+++ app/page.tsx
@@ -27,57 +27,61 @@ export default function HomePage() {
   }
 
   return (
-    <main className="p-8 space-y-4">
-      <img src="/cookie.svg" width={220} alt="Fortune cookie" />
-      <div className="animated-typewriter">
-        <h1 className="text-[30px] font-bold animated-banner">My Techie Cookie.</h1>
-      </div>
-      <h2 className="text-[22px]">
-        What type of coder are you? Click to reveal your fortune:
-      </h2>
-      <div className="flex gap-2">
-        {coders.map((coder) => (
-          <button
-            key={coder}
-            onClick={() => handleClick(coder)}
-            className="px-3 py-1 border rounded bg-gray-200 hover:bg-teal-200 focus:bg-teal-200"
-          >
-            {coder}
-          </button>
-        ))}
-      </div>
-      <div className="cookie-jar">
-        {isCoderAll === true ? (
-          <div>
-            {
-              slips.map((slip: string, index: number) => (
-                <div key={index} className="cookie-slip p-3 pb-2 bg-white mt-4">
-                  <div>
-                    <p className="text-lg leading-[1px]">{slip}</p>
+    <main className="grid grid-col-1">
+      <header className="p-8 space-y-4">
+        <img className="logo" src="/cookie.svg" width={220} alt="Fortune cookie" />
+        <div className="animated-typewriter pt-2 md:pt-4 lg:pt-0">
+          <h1 className="text-[30px] font-bold animated-banner">My Techie Cookie.</h1>
+        </div>
+      </header>
+      <section className="pl-5 pr-5">
+        <h2 className="text-[22px]">
+          What type of coder are you? Click to reveal your fortune:
+        </h2>
+        <div className="flex flex-col lg:flex-row gap-4 text-xl lg:text-base mt-5">
+          {coders.map((coder) => (
+            <button
+              key={coder}
+              onClick={() => handleClick(coder)}
+              className="px-3 py-3 lg:py-1 mb-3 lg:mb-0 border rounded bg-gray-200  lg:bg-amber-200 hover:bg-amber-200 lg:hover:bg-teal-200 focus:bg-amber-200 lg:focus:bg-teal-200"
+            >
+              {coder}
+            </button>
+          ))}
+        </div>
+        <div className="cookie-jar mt-2 lg:mt-10 pb-10">
+          {isCoderAll === true ? (
+            <div>
+              {
+                slips.map((slip: string, index: number) => (
+                  <div key={index} className="cookie-slip p-3 pb-2 bg-white mt-6">
+                    <div>
+                      <p className="text-lg leading-[1px]">{slip}</p>
+                    </div>
+                    <div>
+                      <small className="text-sm text-gray-500">(fullstackMaster)</small>
+                      <span className={`spark ${active ? "active" : ""}`}></span>
+                    </div>
                   </div>
-                  <div>
-                    <small className="text-sm text-gray-500">(fullstackMaster)</small>
-                    <span className={`spark ${active ? "active" : ""}`}></span>
-                  </div>
-                </div>
-              ))
-            }
-          </div>
-        ) : (
-          fortune && (
-            <div className="cookie-slip p-3 pb-2 bg-white mt-4">
-              <div>
-                <p className="text-lg leading-[1px]">{fortune?.message}</p>
-              </div>
-              <div>
-                <small className="text-sm text-gray-500">({fortune?.type})
-                </small>
-                <span className={`spark ${active ? "active" : ""}`}></span>
-              </div>
+                ))
+              }
             </div>
-          )
-        )}
-      </div>
+          ) : (
+            fortune && (
+              <div className="cookie-slip p-3 pb-2 bg-white mt-6">
+                <div>
+                  <p className="text-lg leading-[1px]">{fortune?.message}</p>
+                </div>
+                <div>
+                  <small className="text-sm text-gray-500">({fortune?.type})
+                  </small>
+                  <span className={`spark ${active ? "active" : ""}`}></span>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </section>
     </main >
   );
 }
