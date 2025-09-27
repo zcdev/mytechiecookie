e8ac59f69387a91d40364028f6a760341921e734
zcdev <zcdev@hotmail.com>
2025-09-27 10:33:56 -0700

    refactor: remove randomization of cookie slips to simplify logic and clean up code

--- app/utils/getStorageFortune.ts
+++ app/utils/getStorageFortune.ts
@@ -1,14 +1,12 @@
 import { localFortunes, CoderType, Fortune } from "@/types/fortune";
 
 export function getStorageFortune(coderType: CoderType): Fortune {
-    const slips = localFortunes[coderType];
-    const random = slips[Math.floor(Math.random() * slips.length)];
     const allSlips = Object.values(localFortunes).flat();
 
     return {
         id: Date.now(),
         type: coderType,
-        message: coderType !== "fullstackMaster" ? [random] : allSlips ?? "No local fortune found.",
+        message: coderType === "fullstackMaster" ? allSlips : localFortunes[coderType] ?? "No local fortune found.",
     };
 }
 
